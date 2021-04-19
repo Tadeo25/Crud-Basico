0466b7338071666818bd8522ef70feee8d713196
Tadeo Mocoroa <juan_tadeo25@hotmail.com>
2021-04-19 17:28:33 -0300

    se agrego validacion de error

--- src/AgregarProducto.jsx
+++ src/AgregarProducto.jsx
@@ -1,9 +1,83 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Alert, Button, Container, Form } from "react-bootstrap";
+const AgregarProducto = () => {
+  // crear states
+  const [nombreProducto, setNombreProducto] = useState("");
+  const [precioProducto, setPrecioProducto] = useState(0);
+  const [categoria, setCategoria] = useState("");
+  const [error, setError] = useState(false);
 
-export default function AgregarProducto() {
+  const leerCategoria = (e) =>{
+    setCategoria(e.target.value)
+  }
+
+  const handleSubmit = (e) =>{
+        e.preventDefault()
+    //validaciones
+    if(nombreProducto.trim() !== '' && precioProducto !== '' && categoria !== ''){
+    // si esta bien envio datos del producto a la API
+      setError(false);
+    }else{
+    // si fallo muestro cartel de error
+      setError(true);
+    }
+  }
   return (
-    <>
-    <h1>Agregar Producto</h1>
-    </>
+    <Container>
+      <Form className="my-5" onSubmit={handleSubmit}>
+        <h1 className="my-5">Agregar nuevo producto</h1>
+        <Form.Group>
+          <Form.Label>Nombre del producto*</Form.Label>
+          <Form.Control type="text" placeholder="Café" onChange={(e)=> setNombreProducto(e.target.value)}></Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Precio*</Form.Label>
+          <Form.Control type="number" placeholder="50" onChange={(e)=> setPrecioProducto(e.target.value)}></Form.Control>
+        </Form.Group>
+        <h3 className="text-center mt-4">Categoria</h3>
+        <div className="text-center my-4">
+          <Form.Check
+            inline
+            type="radio"
+            label="Bebida caliente"
+            name="categoria"
+            value='bebida-caliente'
+            onChange={leerCategoria}
+          ></Form.Check>
+          <Form.Check
+            inline
+            type="radio"
+            label="Bebida fria"
+            name="categoria"
+            value='bebida-fria'
+            onChange={leerCategoria}
+          ></Form.Check>
+          <Form.Check
+            inline
+            type="radio"
+            label="Dulce"
+            name="categoria"
+            value='dulce'
+            onChange={leerCategoria}
+          ></Form.Check>
+          <Form.Check
+            inline
+            type="radio"
+            label="Salado"
+            name="categoria"
+            value='salado'
+            onChange={leerCategoria}
+          ></Form.Check>
+        </div>
+        <Button variant='success' type='submit' className="w-100">Guardar</Button>
+        {
+          (error === true)?(
+            <Alert variant='danger' className="my-5">Faltan cargar datos obligatorios</Alert>
+          ): null
+        }
+
+      </Form>
+    </Container>
   );
-}
+};
+export default AgregarProducto;
